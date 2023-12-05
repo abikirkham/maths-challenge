fa7556c2ee622283a9dc3d1893928dddd43c2489
abi kirkham <abbeykirkham9@icloud.com>
2023-12-05 19:58:38 +0000

    added js for question 1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,41 +1,12 @@
-var correctCount = 0;
-    var incorrectCount = 0;
-
-    function generateQuestion() {
-      var num1 = Math.floor(Math.random() * 10) + 1;
-      var num2 = Math.floor(Math.random() * 10) + 1;
-      return {
-        question: num1 + " + " + num2,
-        answer: num1 + num2
-      };
-    }
-
-    function displayQuestion() {
-      var questionObj = generateQuestion();
-      var questionContainer = document.getElementById("question-container");
-      questionContainer.innerHTML = "<p>" + questionObj.question + "</p>";
-      return questionObj.answer;
-    }
-
-    function checkAnswer() {
-      var userAnswer = document.getElementById("user-answer").value;
-      var correctAnswer = displayQuestion();
-
-      if (parseInt(userAnswer) === correctAnswer) {
-        alert("Correct!");
-        correctCount++;
-      } else {
-        alert("Incorrect. The correct answer is " + correctAnswer);
-        incorrectCount++;
-      }
-
-      updateResult();
-    }
-
-    function updateResult() {
-      var resultContainer = document.getElementById("result-container");
-      resultContainer.innerHTML = "Correct: " + correctCount + ", Incorrect: " + incorrectCount;
-    }
-
-    // Display the first question on page load
-    displayQuestion();
+const questions = [
+  {
+    question: "What is 4 x 4?",
+    answers: [
+      { text: "24", correct: false},
+      { text: "8", correct: false},
+      { text: "16", correct: true},
+      { text: "18", correct: false},
+      
+    ]
+  }
+]
